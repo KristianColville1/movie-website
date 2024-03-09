04b8450ef78c4de39aad3e612f94482fa1cf5237
KristianColville1 <kristiancolville1@gmail.com>
2024-03-09 21:43:34 +0000

    Added custom navbar brand and custom toggler

---
+++ src/components/atoms/NavBarBrandLogo/NavBarBrandLogo.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./NavBarBrandLogo.css";
+
+const NavbarBrandLogo = () => {
+    return <h1 className="brand-font">RENTFLIX</h1>;
+};
+
+export default NavbarBrandLogo;
--- src/components/organisms/NavBar/NavBar.js
+++ src/components/organisms/NavBar/NavBar.js
@@ -1,25 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-
+import NavbarBrandLogo from "../../atoms/NavBarBrandLogo/NavBarBrandLogo";
+import CustomNavBarToggle from "../../atoms/CustomNavbarToggle/CustomNavBarToggle";
 /**
- * 
+ *
  * Responsible for the navigation of the website, utilises the react router to manage the different pages
  */
 const NavBar = () => {
+    const [expanded, setExpanded] = useState(false);
+    const toggleNavbar = () => setExpanded((prevExpanded) => !prevExpanded);
+
     return (
-        <Navbar collapseOnSelect expand="lg" className="navbar-dark">
+        <Navbar collapseOnSelect expand="md" className="navbar-dark">
             <Container>
-                <Navbar.Brand href="#" className="brand-font">RENTFLIX</Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbar" />
+                <NavbarBrandLogo />
+                <CustomNavBarToggle onClick={ toggleNavbar} />
                 <Navbar.Collapse id="navbar">
-                    <Nav
-                        className="me-auto my-2 my-lg-0"
-                    >
+                    <Nav className="me-auto my-2 my-lg-0">
+                        <Nav.Link href="#home" className="ms-md-5">
+                            Home
+                        </Nav.Link>
                         <Nav.Link href="#action1">Movies</Nav.Link>
                         <Nav.Link href="#action2">Booking</Nav.Link>
-
                     </Nav>
                 </Navbar.Collapse>
             </Container>
