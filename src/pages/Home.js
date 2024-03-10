16cd173e409b43af953aa0d4129122a0f693a9df
KristianColville1 <kristiancolville1@gmail.com>
2024-03-10 20:56:00 +0000

    Added home page, fetched a random image and tested overlay on diff devices

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Container from "react-bootstrap/Container";
+import axios from "axios";
+import "./Home.css";
 
 const Home = () => {
-    return <></>;
+    const [moviePosterUrl, setMoviePosterUrl] = useState("");
+
+    useEffect(() => {
+        const fetchMovies = async () => {
+            try {
+                const screenWidth = window.innerWidth;
+                const response = await axios.get("assets/data/movies.json");
+                const movies = response.data;
+                const randomMovie =
+                    movies[Math.floor(Math.random() * movies.length)];
+                
+                
+                const imageUrl = screenWidth > 768 ? `https://image.tmdb.org/t/p/original${randomMovie.backdrop_path}` : randomMovie.poster_path;
+                setMoviePosterUrl(imageUrl);
+            } catch (error) {
+                console.error("Failed to fetch movies:", error);
+            }
+        };
+
+        fetchMovies();
+    }, []);
+
+    return (
+        <Container
+            fluid
+            className="d-flex justify-content-end align-items-start position-relative m-0 p-0"
+            style={{ height: "100vh" }}
+        >
+            <div class="overlay"></div>
+            {moviePosterUrl && (
+                <img
+                    src={moviePosterUrl}
+                    className="movie-poster"
+                    alt="Movie Poster"
+                />
+            )}
+        </Container>
+    );
 };
 
 export default Home;
