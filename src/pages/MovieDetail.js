99a9a8ad560202bd18ed55d8ac1c3e154bcc05cd
KristianColville1 <kristiancolville1@gmail.com>
2024-03-13 20:31:34 +0000

    Added better UI to the movie detail page

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,23 +1,94 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useMovies } from "../context/MovieContext";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import "./MovieDetail.css";
 
 const MovieDetail = () => {
+    // fetches the movie context
     const { id } = useParams();
     const { movies } = useMovies();
 
     const movie = movies.find((movie) => movie.id.toString() === id);
 
     return (
-        <div>
+        <Container className="my-5 pb-5">
             {movie ? (
-                <div>
-                    <h2>{movie.title}</h2>
-                </div>
+                <Card className="mb-3">
+                    <Row noGutters>
+                        <Col md={4}>
+                            <Card.Img
+                                variant="top"
+                                src={movie.poster_path}
+                                alt={movie.title}
+                            />
+                        </Col>
+                        <Col md={8}>
+                            <Card.Body>
+                                <Card.Title className="fw-bold fs-1">
+                                    {movie.title}
+                                </Card.Title>
+                                <Card.Text>
+                                    <small className="text-muted">
+                                        Release Date:{" "}
+                                        {new Date(
+                                            movie.release_date
+                                        ).toLocaleDateString("en-US", {
+                                            year: "numeric",
+                                            month: "long",
+                                            day: "numeric",
+                                        })}
+                                    </small>
+                                </Card.Text>
+                                <Card.Text>{movie.overview}</Card.Text>
+                                <Card.Text>
+                                    <strong>Rating:</strong>
+                                    <span className="movie-rating">
+                                        {/* full stars based on rating */}
+                                        {[
+                                            ...Array(
+                                                Math.floor(movie.rating / 2)
+                                            ),
+                                        ].map((_, i) => (
+                                            <i
+                                                key={i}
+                                                className="bx bxs-star"
+                                            ></i>
+                                        ))}
+                                        {/* Half star if rating includes half point */}
+                                        {movie.rating % 2 >= 0.5 ? (
+                                            <i className="bx bxs-star-half"></i>
+                                        ) : null}
+                                        {/* Empty stars for the rest */}
+                                        {[
+                                            ...Array(
+                                                5 - Math.ceil(movie.rating / 2)
+                                            ),
+                                        ].map((_, i) => (
+                                            <i
+                                                key={i + 5}
+                                                className="bx bx-star"
+                                            ></i>
+                                        ))}
+                                    </span>{" "}
+                                    {/* shorten to 1 decimal place*/}
+                                    {movie.rating.toFixed(1)} / 10
+                                </Card.Text>
+                                <Button
+                                    variant="primary"
+                                    href={movie.trailer}
+                                    target="_blank"
+                                >
+                                    Watch Trailer
+                                </Button>
+                            </Card.Body>
+                        </Col>
+                    </Row>
+                </Card>
             ) : (
-                <p>Movie not found</p>
+                <p className="text-center">Movie not found</p>
             )}
-        </div>
+        </Container>
     );
 };
 
