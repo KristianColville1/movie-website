7fc1443927090cc9911389989336267f91faf1df
Kristian Colville <kristiancolville1@gmail.com>
2024-04-10 12:20:32 +0100

    Added SEO element with Helmet

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,6 +4,7 @@ import { Button } from "react-bootstrap";
 import { useMovies } from "../../context/MovieContext";
 import MovieSlider from "../../components/molecules/MovieSlider/MovieSlider";
 import { Link } from "react-router-dom";
+import SEO from "../../utils/SEO";
 import "./Home.css";
 
 /**
@@ -34,50 +35,62 @@ const Home = () => {
     }, [movies]);
 
     return (
-        <Container fluid className="g-0 m-0 p-0">
-            <Container fluid className="m-0 p-0 min-vh-100">
-                <div className="overlay"></div>
-                {moviePosterUrl && (
-                    <img
-                        src={moviePosterUrl}
-                        className="movie-poster"
-                        alt="Movie Poster"
-                    />
-                )}
-                <Container className="movie-contents-container">
-                    {movieInfo && (
-                        <div className="movie-contents">
-                            <h1 className="movie-title">{movieInfo.title}</h1>
-                            <p className="movie-tagline">{movieInfo.tagline}</p>
-                            <Link
-                                to={`/movie/${movieInfo.id}`}
-                                key={movieInfo.id}
-                            >
-                                {" "}
-                                <Button
-                                    variant="info"
-                                    size="lg"
-                                    className="rent-button"
-                                >
-                                    More Info
-                                </Button>
-                            </Link>
-                        </div>
+        <>
+            <SEO
+                title="RENTFLIX, find movies and book today"
+                description="Movie Website for bookings"
+                name="RENTFLIX"
+                type="Website"
+            />
+            <Container fluid className="g-0 m-0 p-0">
+                <Container fluid className="m-0 p-0 min-vh-100">
+                    <div className="overlay"></div>
+                    {moviePosterUrl && (
+                        <img
+                            src={moviePosterUrl}
+                            className="movie-poster"
+                            alt="Movie Poster"
+                        />
                     )}
+                    <Container className="movie-contents-container">
+                        {movieInfo && (
+                            <div className="movie-contents">
+                                <h1 className="movie-title">
+                                    {movieInfo.title}
+                                </h1>
+                                <p className="movie-tagline">
+                                    {movieInfo.tagline}
+                                </p>
+                                <Link
+                                    to={`/movie/${movieInfo.id}`}
+                                    key={movieInfo.id}
+                                >
+                                    {" "}
+                                    <Button
+                                        variant="info"
+                                        size="lg"
+                                        className="rent-button"
+                                    >
+                                        More Info
+                                    </Button>
+                                </Link>
+                            </div>
+                        )}
+                    </Container>
                 </Container>
+
+                <section className="py-5">
+                    <h2 className="text-white text-center">Trending Now</h2>
+                    <MovieSlider movies={trendingMovies} />
+
+                    <h2 className="text-white text-center">New Releases</h2>
+                    <MovieSlider movies={newReleases} />
+
+                    <h2 className="text-white text-center">Other World</h2>
+                    <MovieSlider movies={classics} />
+                </section>
             </Container>
-
-            <section className="py-5">
-                <h2 className="text-white text-center">Trending Now</h2>
-                <MovieSlider movies={trendingMovies} />
-
-                <h2 className="text-white text-center">New Releases</h2>
-                <MovieSlider movies={newReleases} />
-
-                <h2 className="text-white text-center">Other World</h2>
-                <MovieSlider movies={classics} />
-            </section>
-        </Container>
+        </>
     );
 };
 
